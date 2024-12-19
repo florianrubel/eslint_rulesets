0d4fc643e07a766654c099bfeabdcdd27a77fadc
Florian Rubel <florianrubel@display-awesome.com>
2024-12-19 15:02:12 +0100

    add type import rule

--- typescript.js
+++ typescript.js
@@ -3,6 +3,7 @@ export default {
     '@typescript-eslint/no-explicit-any': 'error', // Forbid to use any
     "@typescript-eslint/consistent-type-assertions": "error", // Enforce consistent type assertions (as over <Type>).
     "@typescript-eslint/prefer-optional-chain": "error", // Enforce the use of optional chaining (?.) where applicable.
-    '@typescript-eslint/no-unused-vars': ['error',{ argsIgnorePattern: '^_', varsIgnorePattern: '^_' },], // Forbid unused variables but allow them if they start with _.
-    'import/extensions': ['error', 'ignorePackages', {'ts': 'never','tsx': 'never'}], // don't enforce file extensions on ts and tsx files.
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },], // Forbid unused variables but allow them if they start with _.
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }], // Enforce the type keyword when importing types.
+    'import/extensions': ['error', 'ignorePackages', { 'ts': 'never', 'tsx': 'never' }], // don't enforce file extensions on ts and tsx files.
 }
